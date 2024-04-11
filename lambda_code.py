f36e8c57ca283c90b029f292840b828aa1929409
Shreyas Rao <shreyassureshrao@gmail.com>
2024-04-11 12:44:45 +0530

    added 2 files

---
+++ lambda_code.py
@@ -0,0 +1,9 @@
+import json
+
+def lambda_handler(event, context):
+    # Lambda function logic goes here
+    return 
+    {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
